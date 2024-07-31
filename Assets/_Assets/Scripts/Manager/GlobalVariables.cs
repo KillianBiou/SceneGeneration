f51f8d3cca9fdafefccf460986077ec4c2d6f78e
KillianBiou <62405524+KillianBiou@users.noreply.github.com>
2024-07-31 20:22:24 +0900

    ewrstiopjhgfbvddsfyio

--- Assets/_Assets/Scripts/Manager/GlobalVariables.cs
+++ Assets/_Assets/Scripts/Manager/GlobalVariables.cs
@@ -1,6 +1,8 @@
 using Microsoft.Win32;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using UnityEngine;
 
@@ -65,6 +67,70 @@ public class GlobalVariables : MonoBehaviour
         if (!Directory.Exists(imagesPath))
             Directory.CreateDirectory(imagesPath);
         EndOfGen();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Run();
+        }
+    }
+
+    public void Run()
+    {
+        UnityEngine.Debug.Log(Path.Combine(Application.dataPath, "TripoSR", "InstallRequirement.bat"));
+        // start the child process
+        Process process = new Process();
+
+        // redirect the output stream of the child process.
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.CreateNoWindow = false;
+        process.StartInfo.FileName = Path.Combine(Application.dataPath, "TripoSR", "InstallRequirement.bat");
+        process.StartInfo.Arguments = pythonPath;
+
+        if (!string.IsNullOrEmpty(Path.Combine(Application.dataPath, "TripoSR")))
+        {
+            process.StartInfo.WorkingDirectory = Path.Combine(Application.dataPath, "TripoSR");
+        }
+        else
+        {
+            process.StartInfo.WorkingDirectory = Application.temporaryCachePath; // nb. can only be called on the main thread
+        }
+
+        int exitCode = -1;
+        string output = null;
+
+        try
+        {
+            process.Start();
+            UnityEngine.Debug.Log("Start");
+
+            // do not wait for the child process to exit before
+            // reading to the end of its redirected stream.
+            // process.WaitForExit();
+
+            // read the output stream first and then wait.
+            output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+        }
+        catch (Exception e)
+        {
+            UnityEngine.Debug.LogError("Run error" + e.ToString()); // or throw new Exception
+        }
+        finally
+        {
+            exitCode = process.ExitCode;
+
+            process.Dispose();
+            process = null;
+        }
+
+        // process exitCode/output, call onComplete handlers etc.
+        // ...
+        UnityEngine.Debug.Log(output);
+
     }
 
     public bool SetCurrentApplicationState(ApplicationState newState)
